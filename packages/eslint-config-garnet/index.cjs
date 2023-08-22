1b5f608a5bb25586d4ea3491463983d66131b315
Brett Dorrans <hello@lapidist.net>
2023-08-22 20:03:33 +0100

    feat(eslint-config-garnet): add ESLint config to packages

---
+++ packages/eslint-config-garnet/index.cjs
@@ -0,0 +1,70 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  globals: {
+    jsx: true,
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+    'mdx/code-blocks': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+    'plugin:prettier/recommended',
+    'plugin:compat/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'prettier',
+    'node',
+    'promise',
+    'react',
+    'react-hooks',
+    'compat',
+  ],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.tsx', '.mdx'],
+      },
+    ],
+    'compat/compat': 'error',
+  },
+  overrides: [
+    {
+      files: ['*rc.js', '*.config.js', '*.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+    {
+      files: ['*.mdx', '*.md'],
+      parser: 'eslint-mdx',
+      extends: 'plugin:mdx/recommended',
+    },
+  ],
+};
