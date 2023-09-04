c500c64b4bb610e89ec2e723c88b148716b7a849
Brett Dorrans <hello@lapidist.net>
2023-09-04 16:59:22 +0100

    revert(vue): remove vue and storybook-vue packages, rename storybook-react to storybook

--- packages/vue/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    root: true,
-    extends: ['@lapidist/garnet'],
-    rules: {
-        'react/react-in-jsx-scope': 'off',
-    },
-};
--- packages/vue/src/components/Button/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-    import { ButtonProps } from '@lapidist/garnet-types';
-
-    defineProps<ButtonProps>()
-</script>
-
-<template>
-    <button :type="type">hi</button>
-</template>
