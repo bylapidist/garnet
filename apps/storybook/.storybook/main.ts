3f0998a7113dbef4fa08b9d764fb3bb4362bd2be
Brett Dorrans <hello@lapidist.net>
2023-09-04 17:09:58 +0100

    build(storybook): upgrade storybook

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,20 +1,27 @@
+import { dirname, join } from 'path';
+
+const getAbsolutePath = (value: string): unknown =>
+    dirname(require.resolve(join(value, 'package.json')));
 export default {
-    framework: '@storybook/react-vite',
-    stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+    framework: getAbsolutePath('@storybook/react-vite'),
+    stories: [
+        '../stories/**/*.mdx',
+        '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
     addons: [
-        '@storybook/addon-a11y',
-        '@storybook/addon-docs',
-        '@storybook/addon-links',
-        '@storybook/addon-backgrounds',
-        '@storybook/addon-controls',
-        '@storybook/addon-highlight',
-        '@storybook/addon-measure',
-        '@storybook/addon-outline',
-        '@storybook/addon-toolbars',
-        '@storybook/addon-viewport'
+        getAbsolutePath('@storybook/addon-a11y'),
+        getAbsolutePath('@storybook/addon-docs'),
+        getAbsolutePath('@storybook/addon-links'),
+        getAbsolutePath('@storybook/addon-backgrounds'),
+        getAbsolutePath('@storybook/addon-controls'),
+        getAbsolutePath('@storybook/addon-highlight'),
+        getAbsolutePath('@storybook/addon-measure'),
+        getAbsolutePath('@storybook/addon-outline'),
+        getAbsolutePath('@storybook/addon-toolbars'),
+        getAbsolutePath('@storybook/addon-viewport'),
     ],
     docs: {
         defaultName: 'Documentation',
-        autodocs: true
-    }
+        autodocs: true,
+    },
 };
