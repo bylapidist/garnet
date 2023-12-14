e096ce1e7f9bda6e60f6f4b6ba6407340b72773d
Brett Dorrans <hello@lapidist.net>
2023-12-14 00:27:38 +0000

    wip

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,3 +1,8 @@
+import '@lapidist/garnet-tokens/dist/core.css';
+import '@lapidist/garnet-tokens/dist/light.css';
+import '@lapidist/garnet-tokens/dist/dark.css';
+import '@lapidist/garnet-react/style.css';
+
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
