b8d2a1960f3b99b8b58ea1b6a39ad387cc3048d1
Brett Dorrans <hello@lapidist.net>
2024-04-10 17:42:12 +0100

    feat(text): implement Text variants

--- apps/storybook/stories/components/Text.stories.tsx
+++ apps/storybook/stories/components/Text.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { App, Text } from '@lapidist/garnet-react';
-import type { TextProps } from '@lapidist/garnet-types';
+import type { CommonProps, TextProps } from '@lapidist/garnet-types';
 import type { Meta, StoryObj, StoryFn } from '@storybook/react';
 
 export default {
@@ -9,6 +9,107 @@ export default {
     decorators: [(getStory) => <App>{getStory()}</App>],
 } as Meta;
 
-const Template: StoryFn<TextProps> = (args) => <Text {...args} />;
-export const DefaultText: StoryObj<TextProps> = Template.bind({});
-DefaultText.args = {};
+const defaultArgs: CommonProps<TextProps> = {
+    as: 'p',
+    testId: 'Text',
+    size: 'medium',
+    weight: 'regular',
+    family: 'body',
+};
+
+const Template: StoryFn<CommonProps<TextProps>> = (args) => (
+    <Text {...args}>
+        Nunc porttitor lectus ex, eu pharetra elit placerat non. Suspendisse nec
+        ultrices augue, et varius velit. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia curae.
+    </Text>
+);
+export const SmallSize: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        size: 'small',
+    },
+};
+
+export const MediumSize: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        size: 'medium',
+    },
+};
+
+export const LargeSize: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        size: 'large',
+    },
+};
+
+export const RegularWeight: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        weight: 'regular',
+    },
+};
+
+export const MediumWeight: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        weight: 'medium',
+    },
+};
+
+export const BoldWeight: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        weight: 'bold',
+    },
+};
+
+export const BodyFamily: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        family: 'body',
+    },
+};
+
+export const HeadingFamily: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        family: 'heading',
+    },
+};
+
+export const SerifFamily: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        family: 'serif',
+    },
+};
+
+export const MonoFamily: StoryObj<CommonProps<TextProps>> = {
+    render: Template,
+
+    args: {
+        ...defaultArgs,
+        family: 'mono',
+    },
+};
--- packages/react/src/components/Text/index.tsx
+++ packages/react/src/components/Text/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { TextProps } from '@lapidist/garnet-types';
+import cx from 'classnames';
+import { CommonProps, TextProps } from '@lapidist/garnet-types';
 
 import styles from '@lapidist/garnet-styles/components/text.module.css';
 
-export default function Text(props: React.PropsWithChildren<TextProps>) {
-    return <p className={styles.text}>{JSON.stringify(props)}</p>;
+export default function Text({
+    id,
+    className,
+    as: Component = 'div',
+    testId = 'Text',
+    size = 'medium',
+    family = 'body',
+    weight = 'regular',
+    ...restProps
+}: React.PropsWithChildren<CommonProps<TextProps>>) {
+    return (
+        <Component
+            data-testid={testId}
+            id={id}
+            className={cx(
+                styles.this,
+                styles[`size-${size}`],
+                styles[`family-${family}`],
+                styles[`weight-${weight}`],
+                className,
+            )}
+            {...restProps}
+        />
+    );
 }
