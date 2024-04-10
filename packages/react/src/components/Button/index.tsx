25aa5928e349bed70db8585c8cb7245a5d3cd82b
Brett Dorrans <hello@lapidist.net>
2024-04-10 13:37:06 +0100

    style(button): add whitespace

--- packages/react/src/components/Button/index.tsx
+++ packages/react/src/components/Button/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ButtonProps } from '@lapidist/garnet-types';
 
 import styles from '@lapidist/garnet-styles/components/button.module.css';
+
 export default function Button(props: React.PropsWithChildren<ButtonProps>) {
     return (
         <button className={styles.button} type={props.type}>
