094e72b61f19ee32651b972060710121eca60274
Brett Dorrans <hello@lapidist.net>
2024-04-19 11:26:37 +0100

    build(stylelint): separate stylelint config into stylelint-config-garnet workspace

---
+++ packages/stylelint-config-garnet/index.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: ['stylelint-no-unsupported-browser-features'],
+    extends: [
+        'stylelint-config-recommended',
+        'stylelint-config-css-modules',
+        'stylelint-config-standard-scss',
+    ],
+    rules: {
+        'custom-property-pattern': null,
+    },
+};
